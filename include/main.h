f9e4db2c70d64bd3cfcbf6143f469f9d2431f543
CrazyGuy108 <taylor@hansenfamily.us>
2017-10-24 18:59:02 -0700

    Add code for claw and mobile goal lift
    In case you didn't know, MGL = Mobile Goal Lift. Also corrected a
    couple minor errors in comment wording and mixed tabs and all that.

--- include/main.h
+++ include/main.h
@@ -1,5 +1,5 @@
-// declares the functions that should exist for all c files to use or at least
-//  know about
+// declares the functions, constants, etc. that should exist for all c files to
+//  use or at least know about
 
 #ifndef MAIN_H
 #define MAIN_H
@@ -16,10 +16,14 @@ extern "C" {
 #define DRIVE_FL 3
 #define DRIVE_FR 4
 #define DRIVE_BR 5
+#define CLAW 6
+#define MGL_LEFT 7
+#define MGL_RIGHT 8
+#define SAD_LONELY_PORT 9 // :(
 #define LIFT_2 10
 
 // these last 4 functions down here are what PROS uses internally to do cool
-//  stuff so it's not recommended to call them within actual code
+//  stuff so it's not recommended to call them within the actual code
 
 // main point of execution for the autonomous period
 void autonomous();
--- src/opcontrol.c
+++ src/opcontrol.c
@@ -9,12 +9,14 @@
 #define THRESHOLD 4
 // speed settings for various button-controled parts
 #define LIFT_SPEED 127
+#define CLAW_SPEED 63
+#define MGL_SPEED 63
 
 // returns what was given if the value exceeds the threshold, else 0
 // prevents joystick ghosting
 int threshold(int value)
 {
-	  return abs(value) > THRESHOLD ? value : 0;
+    return abs(value) > THRESHOLD ? value : 0;
 }
 
 // sets the motor at the given channel to the given speed based on whether you
@@ -62,9 +64,16 @@ void operatorControl()
         // gather button input
         bool liftUp = joystickGetDigital(1, 6, JOY_UP);
         bool liftDown = joystickGetDigital(1, 6, JOY_DOWN);
+        bool clawOpen = joystickGetDigital(1, 5, JOY_UP);
+        bool clawClose = joystickGetDigital(1, 5, JOY_DOWN);
+        bool mglUp = joystickGetDigital(1, 8, JOY_UP);
+        bool mglDown = joystickGetDigital(1, 8, JOY_DOWN);
         // set all the other devices accordingly
         buttonControl(LIFT_1, LIFT_SPEED, liftUp, liftDown);
         buttonControl(LIFT_2, -LIFT_SPEED, liftUp, liftDown);
+        buttonControl(CLAW, CLAW_SPEED, clawOpen, clawClose);
+        buttonControl(MGL_LEFT, MGL_SPEED, mglUp, mglDown);
+        buttonControl(MGL_RIGHT, -MGL_SPEED, mglUp, mglDown);
         // wait a bit before receiving input again
         delay(20);
     }
