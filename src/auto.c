698850d0239b7550f16554b1c2cacf6451744184
CrazyGuy108 <taylor@hansenfamily.us>
2017-12-10 16:46:31 -0800

    Improve autonomous
    Stationary goal autonomous fixes:
    * Going too far forwards/backwards.
    * Claw not reliably grabbing the cone.
    * Lift not activating the servo lock.
    * Lift going up/down too much or too little at times.
    
    Added an experimental mobile goal autonomous program that has yet to be
    tested.
    
    The wait time calculations in the turnCW/turnCCW/straight functions
    happened after they set the motors. Now they're calculated before
    setting the motors so that the overhead of calculating all that
    wouldn't affect how far it goes, even if it's just a little bit.

--- src/auto.c
+++ src/auto.c
@@ -8,24 +8,29 @@
 // not in parentheses to take advantage of only doing integer arithmetic
 #define MOTOR_SPEED 5ul/3ul // rot/s
 #define PI 22ul/7ul // overestimated (22/7>PI) because ints always round down
-#define CLAW_TIME 100ul // ms
+#define CLAW_TIME 150ul // ms
 
 // autonomous plans
 static void forwardAndBack();
 static void stationaryGoal();
+static void mobileGoal();
 
+// dt functions require a stop() at the end to allow chaining
 static void turnCW(unsigned int angle, int turnRadius, int leftPower);
 static void turnCCW(unsigned int angle, int turnRadius, int rightPower);
 static void straight(unsigned long distance, int power);
 static void stop();
+// other stuff
 static void lift(direction_t direction, unsigned long waitTime);
 static void claw(direction_t direction);
+static void mgl(direction_t direction, unsigned long waitTime);
 
 // main point of execution for the autonomous period
 void autonomous()
 {
     //forwardAndBack();
     stationaryGoal();
+    //mobileGoal();
 }
 
 void forwardAndBack()
@@ -37,20 +42,53 @@ void forwardAndBack()
 
 void stationaryGoal()
 {
+    stop();
     // start on the middle
     // pick up the cone
     claw(CLOSE);
-    lift(UP, 3500ul);
+    lift(UP, 4200ul);
     // go up to the stationary goal
-    straight(128ul, 127);
+    straight(96ul, 64);
     stop();
     // score the preload
-    lift(DOWN, 1100ul);
+    lift(DOWN, 950ul);
     claw(OPEN);
-    straight(32ul, -127);
-    lift(DOWN, 2400ul);
-    // back up a bit
-    straight(95ul, -127);
+    // back up a bit to fully lower the lift
+    straight(64ul, -64);
+    stop();
+    lift(DOWN, 2800ul);
+}
+
+void mobileGoal()
+{
+    // pick up the cone
+    claw(CLOSE);
+    lift(UP, 1200ul);
+    // drive over to the mobile goal
+    straight(950ul, 127);
+    stop();
+    // put the cone on the mobile goal
+    lift(DOWN, 1200ul);
+    claw(OPEN);
+    straight(32ul, -64);
+    // do a 180
+    turnCW(180, 0, 127);
+    // pick up the mobile goal
+    straight(35ul, -64);
+    stop();
+    mgl(UP, 1300ul);
+    // drive over to the white tape
+    straight(950ul, -127);
+    // align with the 20pt zone
+    turnCCW(45, 0, 64);
+    straight(500ul, 127);
+    turnCW(90, 0, 64);
+    // score the mobile goal into the 20pt zone
+    straight(530ul, -127);
+    stop();
+    mgl(DOWN, 1000ul);
+    // get out of the bumps to give the driver some extra time
+    straight(450ul, 127);
     stop();
 }
 
@@ -69,8 +107,6 @@ void turnCW(unsigned int angle, int turnRadius, int leftPower)
      */
     int rightPower = (((int) turnRadius - (int) BOT_RADIUS) * leftPower) /
         ((int) turnRadius + (int) BOT_RADIUS);
-    setLeftDriveTrain(leftPower);
-    setRightDriveTrain(rightPower);
     /*
      * leftSpeed*T = 2*PI*(turnRadius+BOT_RADIUS)
      * T = 2*PI*(turnRadius+BOT_RADIUS)/leftSpeed
@@ -88,6 +124,8 @@ void turnCW(unsigned int angle, int turnRadius, int leftPower)
      */
     unsigned long waitTime = ((turnRadius + BOT_RADIUS) * 3175ul * angle) /
         (WHEEL_RADIUS * MOTOR_SPEED * 9ul * leftPower);
+    setLeftDriveTrain(leftPower);
+    setRightDriveTrain(rightPower);
     taskDelay(waitTime);
 }
 
@@ -95,17 +133,15 @@ void turnCCW(unsigned int angle, int turnRadius, int rightPower)
 {
     int leftPower = (((int) turnRadius - (int) BOT_RADIUS) * rightPower) /
         ((int) turnRadius + (int) BOT_RADIUS);
-    setLeftDriveTrain(leftPower);
-    setRightDriveTrain(rightPower);
     unsigned long waitTime = ((turnRadius + BOT_RADIUS) * 3175ul * angle) /
         (WHEEL_RADIUS * MOTOR_SPEED * 9ul * rightPower);
+    setLeftDriveTrain(leftPower);
+    setRightDriveTrain(rightPower);
     taskDelay(waitTime);
 }
 
 void straight(unsigned long distance, int power)
 {
-    setLeftDriveTrain(power);
-    setRightDriveTrain(power);
     /*
      * speed = (power/127)*(2*PI*WHEEL_RADIUS*MOTOR_SPEED/(1 rotation))
      * = (power*2*PI*WHEEL_RADIUS*MOTOR_SPEED)/127
@@ -115,6 +151,8 @@ void straight(unsigned long distance, int power)
      */
     unsigned long waitTime = (127000ul * distance) /
         (2ul * WHEEL_RADIUS * MOTOR_SPEED * PI * (unsigned long) abs(power));
+    setLeftDriveTrain(power);
+    setRightDriveTrain(power);
     taskDelay(waitTime);
 }
 
@@ -126,9 +164,11 @@ void stop()
 
 void lift(direction_t direction, unsigned long waitTime)
 {
+    setLiftLock(false);
     setLift(direction);
     taskDelay(waitTime);
     setLift(STOP);
+    setLiftLock(true);
 }
 
 void claw(direction_t direction)
@@ -136,4 +176,11 @@ void claw(direction_t direction)
     setClaw(direction);
     taskDelay(CLAW_TIME);
     setClaw(STOP);
+}
+
+void mgl(direction_t direction, unsigned long waitTime)
+{
+    setMobileGoalLift(direction);
+    taskDelay(waitTime);
+    setMobileGoalLift(STOP);
 }
